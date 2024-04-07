5e7ba140605156d6b49b9256aff114e6c40d3996
mrl00 <m2l_0@outlook.com>
2024-04-07 17:34:23 -0300

    feat: create blockchain struct

--- src/blockchain.jl
+++ src/blockchain.jl
@@ -1,2 +1,26 @@
 module blockchain
+
+include("block.jl")
+include("transaction.jl")
+
+using .block, .transaction, JSON, Dates
+
+export BlockChain, add_block, Transaction
+
+mutable struct BlockChain
+    blocks::Vector{Block}
+    next_index::UInt32
+end
+
+BlockChain(; blocks=Vector{Block}([Block()]), next_index=2) =
+    BlockChain(blocks, next_index)
+
+function add_block(bc::BlockChain, data::String)
+    block = Block(bc.next_index, data, bc.blocks[end].hash)
+    bc.next_index += 1
+    push!(bc.blocks, block)
+end
+
+Base.show(io::IO, bc::BlockChain) = print(io, JSON.json(bc))
+
 end # module blockchain
