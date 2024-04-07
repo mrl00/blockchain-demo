49d4c99cdb66b56046dace8dc3042855b87420cc
mrl00 <m2l_0@outlook.com>
2024-04-07 17:34:59 -0300

    feat: create main module

--- src/main.jl
+++ src/main.jl
@@ -5,7 +5,7 @@ include("blockchain.jl")
 using .blockchain, JSON
 
 bc = BlockChain()
-t = Transaction(from=1, to=1, value=0.34)
+t = Transaction(from=1, to=2, value=0.34)
 
 add_block(bc, repr(t))
 
